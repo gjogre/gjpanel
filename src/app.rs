c36bbeb74573734ec9200578c0dac3934ffc9d81
gjogre <gjogre@pm.me>
2025-04-18 18:19:45 +0300

    made exiting app non-blocking

--- src/app.rs
+++ src/app.rs
@@ -1,62 +1,81 @@
 use crate::config::load_config;
 use crate::widgets::{GJWidget, clock::ClockWidget, weather::WeatherWidget};
-use crossterm::event::{self, KeyCode};
-use ratatui::{Terminal, backend::CrosstermBackend};
-use std::io::Stdout;
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::DefaultTerminal;
 use std::time::{Duration, Instant};
 
-pub fn run_app(
-    terminal: &mut Terminal<CrosstermBackend<Stdout>>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let config = load_config("gjwidgets.toml");
+#[derive(Debug, Default)]
+pub struct App {
+    exit: bool,
+}
 
-    let mut widgets: Vec<(Box<dyn GJWidget>, Duration, Instant)> = vec![
-        (
-            Box::new(ClockWidget::new(config.clock)),
-            Duration::from_secs(1),
-            Instant::now(),
-        ),
-        (
-            Box::new(WeatherWidget::new()),
-            Duration::from_secs(3600),
-            Instant::now(),
-        ),
-    ];
+impl App {
+    pub fn run_app(
+        &mut self,
+        terminal: &mut DefaultTerminal,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let config = load_config("gjwidgets.toml");
 
-    let mut init = true;
-    loop {
-        if event::poll(Duration::from_millis(50))? {
-            if let event::Event::Key(key) = event::read()? {
-                if key.code == KeyCode::Char('q') {
-                    break;
+        let mut widgets: Vec<(Box<dyn GJWidget>, Duration, Instant)> = vec![
+            (
+                Box::new(ClockWidget::new(config.clock)),
+                Duration::from_secs(1),
+                Instant::now(),
+            ),
+            (
+                Box::new(WeatherWidget::new()),
+                Duration::from_secs(3600),
+                Instant::now(),
+            ),
+        ];
+
+        let mut init = true;
+        while !self.exit {
+            for (widget, interval, last_polled) in widgets.iter_mut() {
+                if last_polled.elapsed() >= *interval || init {
+                    widget.poll();
+                    *last_polled = Instant::now();
                 }
             }
+
+            terminal.draw(|f| {
+                let size = f.area();
+                let chunks = ratatui::layout::Layout::default()
+                    .direction(ratatui::layout::Direction::Vertical)
+                    .constraints(vec![
+                        ratatui::layout::Constraint::Length(15),
+                        ratatui::layout::Constraint::Length(30),
+                    ])
+                    .split(size);
+
+                for ((widget, _, _), area) in widgets.iter().zip(chunks.iter()) {
+                    f.render_widget(widget.render(), *area);
+                }
+            })?;
+
+            self.handle_events()?;
+            init = false;
+            std::thread::sleep(Duration::from_millis(200));
         }
 
-        for (widget, interval, last_polled) in widgets.iter_mut() {
-            if last_polled.elapsed() >= *interval || init {
-                widget.poll();
-                *last_polled = Instant::now();
-            }
-        }
-
-        terminal.draw(|f| {
-            let size = f.area();
-            let chunks = ratatui::layout::Layout::default()
-                .direction(ratatui::layout::Direction::Vertical)
-                .constraints(vec![
-                    ratatui::layout::Constraint::Length(15),
-                    ratatui::layout::Constraint::Length(30),
-                ])
-                .split(size);
-
-            for ((widget, _, _), area) in widgets.iter().zip(chunks.iter()) {
-                f.render_widget(widget.render(), *area);
-            }
-        })?;
-        init = false;
-        std::thread::sleep(Duration::from_millis(200));
+        Ok(())
     }
-
-    Ok(())
+    fn handle_events(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            _ => {}
+        }
+    }
+    fn exit(&mut self) {
+        self.exit = true;
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
+use app::App;
+
 mod app;
 mod config;
 mod fonts;
 mod widgets;
 
-use app::run_app;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut terminal = ratatui::init();
-    let res = run_app(&mut terminal);
+    let res = App::default().run_app(&mut terminal);
     ratatui::restore();
     res
 }
