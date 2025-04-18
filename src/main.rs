342d1d74f9eb4a9d5b8195a5c9fabe1b3dc1b232
gjogre <gjogre@pm.me>
2025-04-18 18:07:14 +0300

    And there was a better way for init and restore all along...called... init and restore.

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,12 @@ mod app;
 mod config;
 mod fonts;
 mod widgets;
-use std::io::stdout;
 
 use app::run_app;
-use crossterm::{
-    event::{DisableMouseCapture, EnableMouseCapture},
-    execute,
-    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
-};
-use ratatui::{Terminal, prelude::CrosstermBackend};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    enable_raw_mode()?;
-    let mut stdout = stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-
+    let mut terminal = ratatui::init();
     let res = run_app(&mut terminal);
-
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-    terminal.show_cursor()?;
-
+    ratatui::restore();
     res
 }
